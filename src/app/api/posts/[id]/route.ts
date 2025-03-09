4dec9875af1827700be7ac538cfef7c077ebf1d5
Oleksii Kravchenko <95378730+Alekseyrb@users.noreply.github.com>
2025-03-09 18:00:23 +0200

    fix

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,15 +1,15 @@
-import {NextResponse} from "next/server";
-import {redirect} from "next/navigation";
-import {cookies} from "next/headers";
+// import {NextResponse} from "next/server";
+// import {redirect} from "next/navigation";
+// import {cookies} from "next/headers";
 
-export async function DELETE(req: Request, {params}: { params: { id: string } }) {
-  const id = params.id;
-
-  // logic delete post
-  // redirect('/blog')
-
-  const cookiesList =  await cookies();
-  const coo2 = cookiesList.get('Cookie_2')?.value;
-
-  return NextResponse.json({ id, coo2 });
-}
+// export async function DELETE(req: Request, {params}: { params: { id: string } }) {
+//   const id = params.id;
+//
+//   // logic delete post
+//   // redirect('/blog')
+//
+//   const cookiesList =  await cookies();
+//   const coo2 = cookiesList.get('Cookie_2')?.value;
+//
+//   return NextResponse.json({ id, coo2 });
+// }
